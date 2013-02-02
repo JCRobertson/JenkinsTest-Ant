e0211aeb1e3e13b979bc3a95c565c32243af649c
James Robertson <james@jamesrobertson.org>
2013-02-02 12:33:30 -0500

    add jeff and paul

--- src/SWE437.java
+++ src/SWE437.java
@@ -3,7 +3,10 @@ public class SWE437 {
 	
 	public static void main(String[] args){
 		Student James = new Student("James", 12);
+		Student Paul = new Student("Paul", 437);
+		Student Jeff = new Student("Jeff", 432);
 		System.out.println(James.toString());
-		System.out.println(James.toString());
+		System.out.println(Paul.toString());
+		System.out.println(Jeff.toString());
 	}
 }
