b34bddddc9259ae0a7515343e218c7fe8efd8d5d
James Robertson <james@jamesrobertson.org>
2013-02-02 11:38:03 -0500

    jenkins fetch test

--- src/SWE437.java
+++ src/SWE437.java
@@ -4,5 +4,6 @@ public class SWE437 {
 	public static void main(String[] args){
 		Student James = new Student("James", 12);
 		System.out.println(James.toString());
+		System.out.println(James.toString());
 	}
 }
