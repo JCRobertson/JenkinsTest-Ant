fab8f5b7528da5ea92618008bb889689594a4bd4
James Robertson <ubergeek@jamesrobertson.org>
2013-02-02 00:41:52 -0500

    add equals tests

---
+++ src/StudentTest.java
@@ -0,0 +1,49 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class StudentTest {
+
+	public Student s;
+	public Student s1;
+	public Student s2;
+	
+	@Test (expected = NullPointerException.class)
+	public void nameNotNullTest() {
+		s = new Student(null, 5);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void numberNotLessThanZero(){
+		s = new Student("Joe", -999);
+	}
+	
+	@Test
+	public void studentNameGetter(){
+		s = new Student("James", 12);
+		assertEquals("James", s.getName());
+	}
+
+	@Test
+	public void studentNumberGetter(){
+		s = new Student("James", 12);
+		assertEquals(12, s.getFavoriteNumber());
+	}
+	
+	@Test
+	public void equalsSymmetryTest(){
+		s = new Student("James", 12);
+		s1 = new Student("James", 12);
+		assertTrue(s.equals(s1));
+		assertTrue(s1.equals(s));
+	}
+	
+	@Test
+	public void equalsSymmetryFailTest(){
+		s = new Student("James", 12);
+		s1 = new Student("James", 13);
+		assertFalse(s.equals(s1));
+		assertFalse(s1.equals(s));
+	}
+}
