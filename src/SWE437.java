94aa393f01eb5e290730f0e24594f2833d54559e
James Robertson <james@jamesrobertson.org>
2013-02-02 11:16:12 -0500

    add main to SWE437

--- src/SWE437.java
+++ src/SWE437.java
@@ -1,9 +1,8 @@
-
 public class SWE437 {
 	
 	
-	public static void main(){
+	public static void main(String[] args){
 		Student James = new Student("James", 12);
-		
+		System.out.println(James.toString());
 	}
 }
