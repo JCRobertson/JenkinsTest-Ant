83934a5729ccf4bf3f1d7f7ce29857dbe7f54641
JCRobertson <jrobertq@masonlive.gmu.edu>
2013-02-04 19:23:10 -0500

    add Drew Student

--- src/SWE437.java
+++ src/SWE437.java
@@ -5,6 +5,7 @@ public class SWE437 {
 		Student James = new Student("James", 12);
 		Student Paul = new Student("Paul", 437);
 		Student Jeff = new Student("Jeff", 432);
+		Student Drew = new Student("Drew", 8);
 		System.out.println(James.toString());
 		System.out.println(Paul.toString());
 		System.out.println(Jeff.toString());
